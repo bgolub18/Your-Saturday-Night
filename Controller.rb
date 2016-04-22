709b8ce4be8bff18d842f1fb330957295d390ade
Ben Golub <bgolub@lsoc.org>
2016-04-22 10:30:52 -0500

    new

--- Controller.rb
+++ Controller.rb
@@ -2,3 +2,13 @@ require 'sinatra'
 
 get '/' do
 	erb :index
+end
+POST '/first' do
+	@story = "It\'s Saturday night. You get a text from your friend inviting you to the hottest club in Compton."
+	@question = "Do you slide through with the homies?"
+	@choice1 = "Nah. I\'m soft."
+	@choice2 = "HUNDO"
+	erb :index_2_choices
+end
+POST '/stayingin' do
+	
