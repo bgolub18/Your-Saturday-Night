184cd3261a56cd7fd4d5c41e9359d5db129ed3e7
Ben Golub <bgolub@lsoc.org>
2016-04-22 10:58:30 -0500

    First test

--- Controller.rb
+++ Controller.rb
@@ -1,14 +1,31 @@
 require 'sinatra'
 
 get '/' do
-	erb :index
+	@choice1 = "Yes"
+	@choice2 = "No"
+	erb :intro
 end
-POST '/first' do
+get '/first' do
 	@story = "It\'s Saturday night. You get a text from your friend inviting you to the hottest club in Compton."
 	@question = "Do you slide through with the homies?"
 	@choice1 = "Nah. I\'m soft."
 	@choice2 = "HUNDO"
 	erb :index_2_choices
 end
-POST '/stayingin' do
-	
+get '/stayingin' do
+	@story = "Well, looks like your night is over."
+end
+
+get 'direction/:choice' do
+	case params[:choice]
+	when "Yes"
+		
+	when "Nah. I\'m soft." 
+	@story = "Well, looks like your night is over."
+	erb :ending
+	when "iron"
+	#something
+	end	
+
+end
+
